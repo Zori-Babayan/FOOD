aa525ade72b6c4e024ec2e59d146013bb449e7f0
Zori <list_babayan@mail.ru>
2022-05-16 17:48:28 +0300

    add cards 'menu for the day' using  class

--- js/script.js
+++ js/script.js
@@ -1,59 +1,57 @@
-window.addEventListener('DOMContentLoaded', () => {
-
-    const tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
+window.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent"),
+        tabsParent = document.querySelector(".tabheader__items");
 
     function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
+        tabsContent.forEach((item) => {
+            item.classList.add("hide");
+            item.classList.remove("show", "fade");
         });
 
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
+        tabs.forEach((item) => {
+            item.classList.remove("tabheader__item_active");
         });
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
+        tabsContent[i].classList.add("show", "fade");
+        tabsContent[i].classList.remove("hide");
+        tabs[i].classList.add("tabheader__item_active");
     }
 
     hideTabContent();
     showTabContent();
 
-    tabsParent.addEventListener('click', (event) => {
+    tabsParent.addEventListener("click", (event) => {
         const target = event.target;
 
-        if (target && target.classList.contains('tabheader__item')) {
+        if (target && target.classList.contains("tabheader__item")) {
             tabs.forEach((item, i) => {
                 if (target == item) {
                     hideTabContent();
                     showTabContent(i);
                 }
-            })
-
+            });
         }
     });
 
     //Таймер
-    const deadline = '2022-05-21';
+    const deadline = "2022-05-21";
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
             minutes = Math.floor((t / 1000 / 60) % 60),
             seconds = Math.floor((t / 1000) % 60);
         return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        }
+            total: t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+        };
     }
 
     function getZero(num) {
@@ -66,10 +64,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function setClock(selector, endtime) {
         const timer = document.querySelector(selector),
-            days = timer.querySelector('#days'),
-            hours = timer.querySelector('#hours'),
-            minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds');
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector("#hours"),
+            minutes = timer.querySelector("#minutes"),
+            seconds = timer.querySelector("#seconds");
         timeInterval = setInterval(updateClock, 1000);
 
         updateClock();
@@ -88,74 +86,66 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    setClock('.timer', deadline)
-
-
-
+    setClock(".timer", deadline);
 
     //Modal
 
-    const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
-
+    const modalTrigger = document.querySelectorAll("[data-modal]"),
+        modal = document.querySelector(".modal"),
+        modalCloseBtn = document.querySelector("[data-close]");
 
     function openModal() {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden';
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        document.body.style.overflow = "hidden";
         clearInterval(modalTimerId);
     }
 
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', openModal);
+    modalTrigger.forEach((btn) => {
+        btn.addEventListener("click", openModal);
     });
 
-
     function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        document.body.style.overflow = '';
+        modal.classList.add("hide");
+        modal.classList.remove("show");
+        document.body.style.overflow = "";
     }
 
+    modalCloseBtn.addEventListener("click", closeModal);
 
-    modalCloseBtn.addEventListener('click', closeModal);
-
-
-    modal.addEventListener('click', (e) => {
+    modal.addEventListener("click", (e) => {
         if (e.target === modal) {
             closeModal();
         }
     });
 
-    document.addEventListener('keydown', (e) => {
-        if (e.code === "Escape" && modal.classList.contains('show')) {
+    document.addEventListener("keydown", (e) => {
+        if (e.code === "Escape" && modal.classList.contains("show")) {
             closeModal();
         }
     });
 
-
     // const modalTimerId = setTimeout(openModal, 6000);
 
+    /*  function showModalByScroll() {
+           if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+               openModal();
+               window.removeEventListener('scroll', showModalByScroll);
+           }
+       }
+   
+       window.addEventListener('scroll', showModalByScroll);
+    */
 
-    function showModalByScroll() {
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
-            openModal();
-            window.removeEventListener('scroll', showModalByScroll);
-        }
-    }
-
-    window.addEventListener('scroll', showModalByScroll);
-
-
-    // класс карточки 
+    // класс карточки
     class MenuCard {
-        constructor(src, alt, title, descr, price) {
+        constructor(src, alt, title, descr, price, parentSelector) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.parent = document.querySelector(parentSelector);
             this.transfer = 64;
             this.changeToRUB();
         }
@@ -164,23 +154,48 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         render() {
-            const element = document.createElement('div');
+            const element = document.createElement("div");
             element.innerHTML = `
                 <div class="menu__item">
-                     <img src="img/tabs/vegy.jpg" alt="vegy" />
-                     <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
-                     <div class="menu__item-descr">
-                        Меню "Фитнес" - это новый подход к приготовлению блюд: больше
-                         свежих овощей и фруктов. Продукт активных и здоровых людей. Это
-                        абсолютно новый продукт с оптимальной ценой и высоким качеством!
+                     <img src=${this.src} alt=${this.alt} />
+                     <h3 class="menu__item-subtitle">${this.title}</h3>
+                     <div class="menu__item-descr">${this.descr}
                     </div>
                       <div class="menu__item-divider"></div>
                     <div class="menu__item-price">
                         <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>229</span> грн/день</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
                     </div>
                  </div>
             `;
+            this.parent.append(element);
         }
     }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню 'Фитнес'",
+        "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        35,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        "Меню 'Постное'",
+        "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля,овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        50,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню “Премиум”",
+        "В меню “Премиум” мы используем не только красивый дизайн упаковки,но и качественное исполнение блюд.Красная рыба, морепродукты,фрукты - ресторанное меню без похода в ресторан!",
+        65,
+        ".menu .container"
+    ).render();
 });
